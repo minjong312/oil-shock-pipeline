2baa371cb67a52e81ad421a1f9b9c31b0dcf5d8e
minjong312 <jmk333@mju.ac.kr>
2026-06-05 06:59:10 +0000

    Match precise exact paths for gdelt and exchange rate

--- src/process/spark_join_job.py
+++ src/process/spark_join_job.py
@@ -7,14 +7,16 @@ print("========================================")
 
 spark = SparkSession.builder.appName("GDELT_ExchangeRate_Direct").getOrCreate()
 
-raw_gdelt = spark.read.csv("raw_data/gdelt", sep="\t")
+raw_gdelt = spark.read.csv("data/*.export.CSV", sep="\t")
+
 gdelt_df = raw_gdelt.select(
     col("_c1").alias("SQLDATE"),
     col("_c29").cast("int").alias("QuadClass"),
     col("_c30").cast("float").alias("GoldsteinScale")
 )
 
-raw_exchange = spark.read.csv("exchange_rate", header=True)
+raw_exchange = spark.read.csv("raw_data/year=*/month=*/day=*/exchange_rate.csv", header=True)
+
 exchange_df = raw_exchange.select(
     col("Date"),
     col("Close").cast("float").alias("ExchangeRate")
